b1c5793658a9860dea360d79727679cba8932de7
bokarmy <88885@glr.nl>
2023-01-18 16:20:05 +0100

    js quiz1 fortnite change

--- index.js
+++ index.js
@@ -1,229 +1,113 @@
 const questions = [
     {
-        question: "How many days makes a week ?",
-        optionA: "10 days",
-        optionB: "14 days",
-        optionC: "5 days",
-        optionD: "7 days",
-        correctOption: "optionD"
+        question: "When was Fortnite released? ",
+        optionA: "In 2015",
+        optionB: "In 2016",
+        optionC: "In 2017",
+        optionD: "In 2018",
+        correctOption: "optionC"
     },
 
     {
-        question: "How many players are allowed on a soccer pitch ?",
-        optionA: "10 players",
-        optionB: "11 players",
-        optionC: "420 players",
-        optionD: "69 players",
+        question: "What's the in-game currency ?",
+        optionA: "Robux",
+        optionB: "V-Bucks",
+        optionC: "R6 points",
+        optionD: "Euro's",
         correctOption: "optionB"
     },
 
     {
-        question: "Who was the first President of USA ?",
+        question: "What's the name of The Cube ?",
         optionA: "Donald Trump",
-        optionB: "Barack Obama",
-        optionC: "Abraham Lincoln",
+        optionB: "Kevin",
+        optionC: "Ninja",
         optionD: "Joe mama",
-        correctOption: "optionD"
-    },
-
-    {
-        question: "30 days has ______ ?",
-        optionA: "January",
-        optionB: "December",
-        optionC: "June",
-        optionD: "August",
-        correctOption: "optionC"
-    },
-
-    {
-        question: "How manay hours can be found in a day ?",
-        optionA: "30 hours",
-        optionB: "38 hours",
-        optionC: "48 hours",
-        optionD: "24 hours",
-        correctOption: "optionD"
-    },
-
-    {
-        question: "Which is the longest river in the world ?",
-        optionA: "River Nile",
-        optionB: "Long River",
-        optionC: "River Niger",
-        optionD: "Lake Chad",
-        correctOption: "optionA"
-    },
-
-    {
-        question: "_____ is the hottest Continent on Earth ?",
-        optionA: "Oceania",
-        optionB: "Antarctica",
-        optionC: "Africa",
-        optionD: "North America",
-        correctOption: "optionC"
-    },
-
-    {
-        question: "Which country is the largest in the world ?",
-        optionA: "Russia",
-        optionB: "Canada",
-        optionC: "Africa",
-        optionD: "Egypt",
-        correctOption: "optionA"
-    },
-
-    {
-        question: "Which of these numbers is an odd number ?",
-        optionA: "Ten",
-        optionB: "Twelve",
-        optionC: "Eight",
-        optionD: "Eleven",
-        correctOption: "optionD"
-    },
-
-    {
-        question: `"You Can't see me" is a popular saying by`,
-        optionA: "Eminem",
-        optionB: "Bill Gates",
-        optionC: "Chris Brown",
-        optionD: "John Cena",
-        correctOption: "optionD"
-    },
-
-    {
-        question: "Where is the world tallest building located ?",
-        optionA: "Africa",
-        optionB: "California",
-        optionC: "Dubai",
-        optionD: "Italy",
-        correctOption: "optionC"
-    },
-
-    {
-        question: "The longest river in the United Kingdom is ?",
-        optionA: "River Severn",
-        optionB: "River Mersey",
-        optionC: "River Trent",
-        optionD: "River Tweed",
-        correctOption: "optionA"
-    },
-
-
-    {
-        question: "How many permanent teeth does a dog have ?",
-        optionA: "38",
-        optionB: "42",
-        optionC: "40",
-        optionD: "36",
         correctOption: "optionB"
     },
 
     {
-        question: "Which national team won the football World cup in 2018 ?",
-        optionA: "England",
-        optionB: "Brazil",
-        optionC: "Germany",
-        optionD: "France",
-        correctOption: "optionD"
-    },
-
-    {
-        question: "Which US state was Donald Trump Born ?",
-        optionA: "New York",
-        optionB: "California",
-        optionC: "New Jersey",
-        optionD: "Los Angeles",
-        correctOption: "optionA"
-    },
-
-    {
-        question: "How man states does Nigeria have ?",
-        optionA: "24",
-        optionB: "30",
-        optionC: "36",
-        optionD: "37",
+        question: "When came the first Battle Pass ?",
+        optionA: "Season 3",
+        optionB: "Season 1",
+        optionC: "Season 2",
+        optionD: "Season 0",
         correctOption: "optionC"
     },
 
     {
-        question: "____ is the capital of Nigeria ?",
-        optionA: "Abuja",
-        optionB: "Lagos",
-        optionC: "Calabar",
-        optionD: "Kano",
+        question: "How many default skins are when you start the game?",
+        optionA: "3",
+        optionB: "2",
+        optionC: "7",
+        optionD: "8",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "What haoppend to the cube at the end of Season 5",
+        optionA: "It teleported",
+        optionB: "Removed",
+        optionC: "It flew away",
+        optionD: "Destroyed",
         correctOption: "optionA"
     },
 
     {
-        question: "Los Angeles is also known as ?",
-        optionA: "Angels City",
-        optionB: "Shining city",
-        optionC: "City of Angels",
-        optionD: "Lost Angels",
+        question: "What is the first vehicle to be added to the game ?",
+        optionA: "Car",
+        optionB: "Plane",
+        optionC: "Shopping cart",
+        optionD: "Golf car",
         correctOption: "optionC"
     },
 
     {
-        question: "What is the capital of Germany ?",
-        optionA: "Georgia",
-        optionB: "Missouri",
-        optionC: "Oklahoma",
-        optionD: "Berlin",
+        question: "What rarity is the Raptor skin ?",
+        optionA: "Common",
+        optionB: "Green",
+        optionC: "Purple",
+        optionD: "Legendary",
         correctOption: "optionD"
     },
 
     {
-        question: "How many sides does an hexagon have ?",
-        optionA: "Six",
-        optionB: "Sevene",
-        optionC: "Four",
-        optionD: "Five",
-        correctOption: "optionA"
+        question: "How much damage is caused from a blue AR head shot ?",
+        optionA: "44",
+        optionB: "55",
+        optionC: "66",
+        optionD: "67",
+        correctOption: "optionC"
     },
 
     {
-        question: "How many planets are currently in the solar system ?",
-        optionA: "Eleven",
-        optionB: "Seven",
-        optionC: "Nine",
-        optionD: "Eight",
-        correctOption: "optionD"
-    },
-
-    {
-        question: "Which Planet is the hottest ?",
-        optionA: "Jupitar",
-        optionB: "Mercury",
-        optionC: "Earth",
-        optionD: "Venus",
+        question: "What is the rarest Skin in the game ? ",
+        optionA: "Ikonik",
+        optionB: "Renegade Raider",
+        optionC: "Black Knight",
+        optionD: "Galaxy Skin",
         correctOption: "optionB"
     },
 
     {
-        question: "where is the smallest bone in human body located?",
-        optionA: "Toes",
-        optionB: "Ears",
-        optionC: "Fingers",
-        optionD: "Nose",
-        correctOption: "optionB"
-    },
-
-    {
-        question: "How many hearts does an Octopus have ?",
-        optionA: "One",
-        optionB: "Two",
-        optionC: "Three",
-        optionD: "Four",
+        question: "What company made Fortnite ?",
+        optionA: "Ubisoft",
+        optionB: "Rockstar Games",
+        optionC: "Epic Games",
+        optionD: "Nintendo",
         correctOption: "optionC"
     },
 
     {
-        question: "How many teeth does an adult human have ?",
-        optionA: "28",
-        optionB: "30",
-        optionC: "32",
-        optionD: "36",
-        correctOption: "optionC"
-    }
+        question: "Which was the last skin in season 4 battle pas ?",
+        optionA: "Omega ",
+        optionB: " Black Knight",
+        optionC: "Carbide",
+        optionD: "Valor ",
+        correctOption: "optionA"
+    },
+
+
 
 ]
 
