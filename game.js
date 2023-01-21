b2255466b2772c9847a016d6d92ac87674a88a78
Osman Karapinar <88885@glr.nl>
2023-01-21 19:09:54 +0100

    nieuwe quiz + style changes

--- game.js
+++ game.js
@@ -11,46 +11,104 @@ let availableQuesions = [];
 
 let questions = [
   {
-    question: "Inside which HTML element do we put the JavaScript??",
-    choice1: "ahllo",
-    choice2: "<javascript>",
-    choice3: "<js>",
-    choice4: "<scripting>",
-    answer: 1
-  },
-  {
-    question:
-      "What is the correct syntax for referring to an external script called 'xxx.js'?",
-    choice1: "<script href='xxx.js'>",
-    choice2: "<script name='xxx.js'>",
-    choice3: "<script src='xxx.js'>",
-    choice4: "<script file='xxx.js'>",
+    question: "When was Fortnite released? ",
+    choice1: "In 2015",
+    choice2: "In 2016",
+    choice3: "In 2017",
+    choice4: "In 2I018",
     answer: 3
   },
   {
-    question: " How do you write 'Hello World' in an alert box?",
-    choice1: "msgBox('Hello World');",
-    choice2: "alertBox('Hello World');",
-    choice3: "msg('Hello World');",
-    choice4: "alert('Hello World');",
+    question:
+      "What's the in-game currency ?",
+    choice1: "Robux",
+    choice2: "V-Bucks",
+    choice3: "R6 points",
+    choice4: "Euro's",
+    answer: 2
+  },
+  {
+    question: " What's the name of The Cube ?",
+    choice1: "Donald Trump",
+    choice2: "Kevin",
+    choice3: "Ninja",
+    choice4: "Joe mama",
+    answer: 2
+  },
+  {
+    question: " When came the first Battle Pass ?",
+    choice1: "Season 3",
+    choice2: "Season 1",
+    choice3: "Season 0",
+    choice4: "Season 2",
     answer: 4
   },
   {
-    question: " How do you write 'Hello World' in an alert box?",
-    choice1: "msgBox('Hello World');",
-    choice2: "alertBox('Hello World');",
-    choice3: "msg('Hello World');",
-    choice4: "alert('Hello World');",
+    question: " How many default skins are when you start the game?",
+    choice1: "2",
+    choice2: "4",
+    choice3: "6",
+    choice4: "8",
     answer: 4
   },
   {
-    question: " How do you write 'Hello World' in an alert box?",
-    choice1: "msgBox('Hello World');",
-    choice2: "alertBox('Hello World');",
-    choice3: "msg('Hello World');",
-    choice4: "alert('Hello World');",
-    answer: 4
+    question: " What happend to the cube at the end of Season 5",
+    choice1: "It teleported",
+    choice2: "Removed",
+    choice3: "It flew away",
+    choice4: "Destroyed",
+    answer: 1
   },
+  {
+    question: "What is the first vehicle to be added to the game ?",
+    choice1: "Car",
+    choice2: "Plane",
+    choice3: "Shopping cart",
+    choice4: "Golf car",
+    answer: 3
+},
+{
+  question: "What rarity is the Raptor skin ?",
+  choice1: "Common",
+  choice2: "Green",
+  choice3: "Purple",
+  choice4: "Legendary",
+  answer: 4
+},
+{
+  question: "How much damage is caused from a blue AR head shot ?",
+  choice1: "44",
+  choice2: "55",
+  choice3: "66",
+  choice4: "67",
+  answer: 3
+},
+
+{
+  question: "What is the rarest Skin in the game ? ",
+  choice1: "Ikonik",
+  choice2: "Renegade Raider",
+  choice3: "Black Knight",
+  choice4: "Galaxy Skin",
+  answer: 2
+},
+{
+  question: "What company made Fortnite ?",
+  choice1: "Ubisoft",
+  choice2: "Rockstar Games",
+  choice3: "Epic Games",
+  choice4: "Nintendo",
+  answer: 3
+},
+{
+  question: "Which was the last skin in season 4 battle pass ?",
+  choice1: "Omega ",
+  choice2: " Black Knight",
+  choice3: "Carbide",
+  choice4: "Valor ",
+  answer: 1 
+},
+
 ];
 
 //CONSTANTS
@@ -118,3 +176,4 @@ incrementScore = num => {
 };
 
 startGame();
+
